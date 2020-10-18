0df4ec0147317f1cd85047178cfa75fa0494ad7a
Christian Peralta <christianipc.1987@gmail.com>
2020-10-18 03:30:28 -0300

    Agrego la sección de contactos

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Banner from './componentes/Banner';
 import Footer from './componentes/Footer';
 import Header from './componentes/Header';
 import Trabajos from './componentes/Trabajos';
+import Contacto from './componentes/Contacto';
 import './scss/estilos.scss'
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Banner />
       <main className="main">
         <Trabajos />
+        <Contacto />
       </main>
       <Footer />
     </div>
